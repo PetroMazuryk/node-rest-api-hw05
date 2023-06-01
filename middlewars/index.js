fc4e2f940eba75f2acf2b03b462ecbc454f71129
PetroMazuryk <109526725+PetroMazuryk@users.noreply.github.com>
2023-06-01 18:03:11 +0300

    add upload.js

--- middlewars/index.js
+++ middlewars/index.js
@@ -2,10 +2,12 @@ const validateBody = require("./validateBody");
 const isValidId = require("./isValidId");
 const authenticate = require("./authenticate");
 const validateBodyFavorite = require("./validateBodyFavorite");
+const upload = require("./upload");
 
 module.exports = {
   validateBody,
   isValidId,
   authenticate,
   validateBodyFavorite,
+  upload,
 };
