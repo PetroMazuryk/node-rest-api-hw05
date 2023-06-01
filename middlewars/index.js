efe2389a0f64612ba48e3ffdba4d135f3dcacccc
PetroMazuryk <109526725+PetroMazuryk@users.noreply.github.com>
2023-06-01 17:36:53 +0300

    start

---
+++ middlewars/index.js
@@ -0,0 +1,11 @@
+const validateBody = require("./validateBody");
+const isValidId = require("./isValidId");
+const authenticate = require("./authenticate");
+const validateBodyFavorite = require("./validateBodyFavorite");
+
+module.exports = {
+  validateBody,
+  isValidId,
+  authenticate,
+  validateBodyFavorite,
+};
